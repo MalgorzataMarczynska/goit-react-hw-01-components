2183b1e76aba4687bf1eaea037711003a83989de
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-01-27 22:56:27 +0100

    user profile feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import user from './profile/user.json';
+import { Profile } from './profile/Profile.js';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
+        backgroundColor: 'lightgray',
       }}
     >
-      React homework template
+      React User Profile
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
