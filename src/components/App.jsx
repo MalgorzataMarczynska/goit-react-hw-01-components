9169f824160fa3a7ee607e5517f5d0a67a8b287c
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-01-29 18:40:44 +0100

    transaction feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from './profile/user.json';
 import data from './statistics/data.json';
 import friends from './friendlist/friends.json';
+import transactions from './transaction_history/transactions.json';
 import { Profile } from './profile/Profile.js';
 import { Statistics } from './statistics/Statistics.js';
-import { FriendList } from './friendlist/FriendList';
-import { FriendListItem } from './friendlist_item/FriendListItem';
+import { FriendList } from './friendlist/FriendList.js';
+import { FriendListItem } from './friendlist_item/FriendListItem.js';
+import { TransactionHistory } from './transaction_history/TransactionHistory.js';
 
 export const App = () => {
   return (
@@ -32,7 +34,7 @@ export const App = () => {
       <FriendList friends={friends}>
         <FriendListItem friends={friends} />
       </FriendList>
-      {/* <FriendListItem friends={friends} /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
